dbebfb3e8c3b521e9fea3d0212e52b8a9a01ab12
franfj <franjrg94@gmail.com>
2018-08-06 18:43:10 +0200

    Test Trie data load

---
+++ src/app/Test.java
@@ -0,0 +1,35 @@
+package app;
+
+import io.github.franfj.ghost.Trie;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+class Test {
+
+    public static void main(String[] args) {
+
+        Trie testTrie = new Trie();
+
+        try {
+            File file = new File("resources/ghostGameDict.txt");
+            FileReader fileReader = new FileReader(file);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                testTrie.addWord(line.trim());
+            }
+            fileReader.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(testTrie);
+    }
+
+
+}
